8f28e3a89f3d81a24efffd33141abc62a0632c73
Belen Sanchez <belensezrami@gmail.com>
2023-05-28 20:44:00 +0200

    conflictos solveados

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,17 @@
-<<<<<<< HEAD
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navigation from './components/Navigation/Navigation';
-
-=======
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
-
 import AppRoutes from './routes/AppRoutes'
->>>>>>> 5bc0b0bdc634de4212193434d1adeae9953c9f47
 
 function App() {
 
   return (
-<<<<<<< HEAD
-    <Navigation />
-  );
-=======
-    <AppRoutes />
-  )
->>>>>>> 5bc0b0bdc634de4212193434d1adeae9953c9f47
+    <div>
+      <Navigation />
+      <AppRoutes />
+    </div>
+    
+  ),
 }
 
 export default App

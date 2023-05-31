272f7a21ca9a6a35daf952393e34963ec5afb666
Paula <94paulala@gmail.com>
2023-05-31 15:43:13 +0200

    pagina y rutas de crear carta

---
+++ src/pages/CreateCardPage/CreateCardPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const CreateCardPage = () => {
+    return (
+        <div>
+            <h1>Crea tus cartas</h1>
+            {/* <CreateCardForm /> */}
+        </div>
+    )
+}
+
+export default CreateCardPage
+
+
+
+
+

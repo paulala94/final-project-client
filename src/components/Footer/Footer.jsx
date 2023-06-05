3ca74c1d28cf13972acafd1de0963a06327bbe90
Paula <94paulala@gmail.com>
2023-06-05 13:02:00 +0200

    footer hecho

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Footer.css'
+
+
+const Footer = () => {
+    return (
+
+        <footer className="footer">
+            <p>Desarrollado por <Link to='/nosotras'>Sacabó Team</Link> </p>
+        </footer>
+
+
+
+    )
+}
+
+export default Footer
---
+++ src/components/FooterMobile/FooterMobile.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const FooterDesktop = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 1200 205.38">
+      <defs>
+        <style>{`.g{fill:url(#f);}`}</style>
+        <linearGradient id="f" x1="53.01" y1="316.24" x2="1140.47" y2="-9.47" gradientTransform="translate(0 -2.62)" gradientUnits="userSpaceOnUse">
+          <stop offset="0" stopColor="#f77e21" />
+          <stop offset="1" stopColor="#f75e66" />
+        </linearGradient>
+      </defs>
+      <g id="a" />
+      <g id="b">
+        <g id="c">
+          <g id="d">
+            <g id="e">
+              <path className="g" d="M0,94.19v111.19H1200V25.74c-67.45,25.58-138.67,38.68-210.21,38.99-37.13,.16-74.35-3.13-111.18-9.9-58-10.79-114.16-30.13-172-41.86-82.39-16.72-168.19-17.73-250.45-.39-79.94,16.81-162.83,57.81-241.82,78.64C144.29,109.7,67.81,117.31,0,94.19Z" />
+            </g>
+          </g>
+        </g>
+      </g>
+    </svg>
+  )
+}
+
+export default FooterDesktop

3f92a85f1abdfa56fcdcfa1dadbcf1cff2a3ec46
Belen Sanchez <belensezrami@gmail.com>
2023-06-05 09:36:22 +0200

    navbar bonita y presiosa

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -16,7 +16,7 @@ const Navigation = () => {
 
   return (
 
-    <>
+  <>
 
       <div className='wavesMobile'>
         <WavesMovile />
@@ -26,7 +26,7 @@ const Navigation = () => {
         <WavesDesktop />
       </div>
 
-      <Navbar bg="transparent" className='navbar navbar-expand-md' expand='md'>
+      <Navbar bg="transparent" className='navbar navbar-expand-md nav-desktop'  expand='md'>
 
         <Container className="d-flex align-items-start justify-content-between">
           <div>
@@ -35,118 +35,151 @@ const Navigation = () => {
             </Navbar.Brand>
           </div>
 
-          <div className='nav-desktop'>
-            <Nav className="me-auto">
+          <div>
+              <Nav className="me-auto">
 
-              <Nav.Link as='span'>
-                <Link to="/juego">Juego</Link>
-              </Nav.Link>
+                <Nav.Link as='span'>
+                  <Link to="/juego">Juego</Link>
+                </Nav.Link>
 
-              <Nav.Link as='span'>
-                <Link to="/crear-cartas">Crear cartas</Link>
-              </Nav.Link>
-              <Nav.Link as='span'>
-                <Link to="/crear-mazos">Crear mazos</Link>
-              </Nav.Link>
+                <Nav.Link as='span'>
+                  <Link to="/crear-cartas">Crear cartas</Link>
+                </Nav.Link>
 
-              <Nav.Link as='span'>
-                <Link to="/todas-las-cartas">Todas las cartas</Link>
-              </Nav.Link>
-              {
-                user
-                  ?
-                  <>
-                    <Nav.Link as='span' className='pointer' onClick={logout}>Cerrar Sesión</Nav.Link>
+                <Nav.Link as='span'>
+                  <Link to="/crear-mazos">Crear mazos</Link>
+                </Nav.Link>
 
-                    <Nav.Link as='span'>
-                      <Link to="/perfil">{user.username}</Link>
-                    </Nav.Link>
-                  </>
-                  :
-                  <>
-                    <Nav.Link as='span'>
-                      <Link to="/registro">Registro</Link>
-                    </Nav.Link>
+                <Nav.Link as='span'>
+                  <Link to="/todas-las-cartas">Todas las cartas</Link>
+                </Nav.Link>
 
-                    <Nav.Link as='span'>
-                      <Link to="/login">Login</Link>
-                    </Nav.Link>
-                  </>
-              }
+                {
+                  user
+                    ?
+                    <>
+                      <Nav.Link as='span'>
+                        <Link to="/perfil">{user.username}</Link>
+                      </Nav.Link>
 
-            </Nav>
-          </div>
+                      <Nav.Link as='span' className='pointer' onClick={logout}>Cerrar Sesión</Nav.Link>
+                    </>
+                    :
+                    <>
+                      <Nav.Link as='span'>
+                        <Link to="/registro">Registro</Link>
+                      </Nav.Link>
 
-          <div className='nav-mobile'>
-            <>
-              <div className="mb-2" >
-                {['start'].map(
-                  (direction) => (
-                    <DropdownButton
-                      as={ButtonGroup}
-                      key={direction}
-                      id={`dropdown-button-drop-${direction}`}
-                      drop={direction}
-                      title={<FontAwesomeIcon icon={faBars} style={{ color: "#fff", }}
-                        className='navbar-bt' />}
-                    >
-                      <Dropdown.Item as='span' eventKey="1" >
-                        <Link to="/juego">Juego</Link>
-                      </Dropdown.Item>
-                      <Dropdown.Item as='span' eventKey="2">
-                        <Link to="/crear-cartas">Crear cartas</Link>
-                      </Dropdown.Item>
-                      <Dropdown.Item as='span' eventKey="3" >
-                        <Link to="/crear-mazos">Crear mazos</Link>
-                      </Dropdown.Item>
-                      <Dropdown.Item as='span' eventKey="4">
-                        <Link to="/todos-los-mazos">Todos los mazos</Link>
-                      </Dropdown.Item>
-                      <Dropdown.Item as='span' eventKey="5">
-                        <Link to="/todas-las-cartas">Todas las cartas</Link>
-                      </Dropdown.Item>
+                      <Nav.Link as='span'>
+                        <Link to="/login">Login</Link>
+                      </Nav.Link>
+                    </>
+                }
 
-                      {
-                        user
-                          ?
-                          <>
+              </Nav>
 
-                            <Dropdown.Item as='span' eventKey="6">
-                              <Link to="/perfil">{user.username}</Link>
-                            </Dropdown.Item>
-
-                            <Dropdown.Divider />
-
-                            <Dropdown.Item as='span' className='pointer' onClick={logout}>Cerrar Sesión</Dropdown.Item>
-                          </>
-                          :
-                          <>
-                            <Dropdown.Item as='span' eventKey="7">
-                              <Link to="/registro">Registro</Link>
-                            </Dropdown.Item>
-                            <Dropdown.Item as='span' eventKey="8">
-                              <Link to="/login">Login</Link>
-                            </Dropdown.Item>
-
-                          </>
-                      }
-
-                    </DropdownButton>
-                  ),
-
-                )}
-
-              </div>
-
-            </>
-
-          </div>
+            </div>
 
         </Container>
 
       </Navbar>
 
-    </>
+
+      <Navbar bg="transparent" className='navbar  nav-mobile'  >
+
+      <Container className="d-flex align-items-start justify-content-between">
+
+        <div>
+          <Navbar.Brand >
+            <Link to="/">FINAL-PROJECT</Link>
+          </Navbar.Brand>
+        </div>
+
+        <div>
+
+            <Nav className="me-auto">
+
+              <div className='nav-mobile'>
+
+                <>
+
+                  <div className="mb-2" >
+                    {['start'].map(
+                      (direction) => (
+                        <DropdownButton
+                          as={ButtonGroup}
+                          key={direction}
+                          id={`dropdown-button-drop-${direction}`}
+                          drop={direction}
+                          title={<FontAwesomeIcon icon={faBars} style={{ color: "#fff", }}
+                            className='navbar-bt' />}
+                        >
+                          <Dropdown.Item as='span' eventKey="1" >
+                            <Link to="/juego">Juego</Link>
+                          </Dropdown.Item>
+
+                          <Dropdown.Item as='span' eventKey="2">
+                            <Link to="/crear-cartas">Crear cartas</Link>
+                          </Dropdown.Item>
+
+                          <Dropdown.Item as='span' eventKey="3" >
+                            <Link to="/crear-mazos">Crear mazos</Link>
+                          </Dropdown.Item>
+
+                          <Dropdown.Item as='span' eventKey="4">
+                            <Link to="/todos-los-mazos">Todos los mazos</Link>
+                          </Dropdown.Item>
+
+                          <Dropdown.Item as='span' eventKey="5">
+                            <Link to="/todas-las-cartas">Todas las cartas</Link>
+                          </Dropdown.Item>
+
+                          {
+                            user
+                              ?
+                              <>
+
+                                <Dropdown.Item as='span' eventKey="6">
+                                  <Link to="/perfil">{user.username}</Link>
+                                </Dropdown.Item>
+
+                                <Dropdown.Divider />
+
+                                <Dropdown.Item as='span' className='pointer' onClick={logout}>Cerrar Sesión</Dropdown.Item>
+                              </>
+                              :
+                              <>
+                                <Dropdown.Item as='span' eventKey="7">
+                                  <Link to="/registro">Registro</Link>
+                                </Dropdown.Item>
+                                <Dropdown.Item as='span' eventKey="8">
+                                  <Link to="/login">Login</Link>
+                                </Dropdown.Item>
+
+                              </>
+                          }
+
+                        </DropdownButton>
+                      ),
+
+                    )}
+
+                  </div>
+
+                </>
+
+              </div>
+
+            </Nav>
+
+          </div>
+
+      </Container>
+
+    </Navbar>
+
+  </>
+  
   )
 
 }
